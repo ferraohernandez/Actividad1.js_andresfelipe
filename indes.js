452c3bcb34ae7c79313174b697abf25ea5cd869f
ferraohernandez <fferraohernandez@gmail.com>
2023-08-01 20:01:18 -0500

    Actividad1

---
+++ indes.js
@@ -0,0 +1,8 @@
+let firstName = "Andres",
+lastName = "Hernandez Ferrao",
+yearbith = 1999,
+city = "Medellin";
+
+let oldyear= 24
+
+console.log("hola, mi nombres es: " + firstName + " "+ lastName+ ", vivo en "+ city+" y tengo " + oldyear +" años");
